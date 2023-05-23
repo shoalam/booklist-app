84ad1c910aec8d69462b8f7b227e51e6364171ad
shoalam <shoalamdu@gmail.com>
2023-05-24 01:35:29 +0600

    Project updated

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+const bookList = document.querySelector('.book-list');
+//const title = document.querySelector('#title').value;
+const title = 'This is title';
+//const author = document.querySelector('#author').value;
+const author = "This is author";
+//const isbn = document.querySelector('#isbn').value;
+const isbn = 10;
+
+//console.log(bookList);
+let parent = "<tr>"
+let child = `
+    <td>${title}</td>
+    <td>${author}</td>
+    <td>${isbn}</td>
+    <td></td>
+`;
+parent +="</tr>";
+
+parent.appendChild(child);
+
+bookList.innerHTML = parent;
+
